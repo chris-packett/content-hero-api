59637e2dd1034ac02a4253c8c87bf624dc8e86b3
Chris Packett <chrispackett1998@gmail.com>
2020-01-11 23:26:49 -0500

    Added projects controller get endpoint.

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,40 @@
+﻿using ContentHeroApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContentHeroApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public List<ProjectModel> GetAllProjects()
+        {
+            return new List<ProjectModel>()
+            {
+                new ProjectModel()
+                {
+                    Id = 1,
+                    Name = "Massey Yacht's Website",
+                    Status = "Draft",
+                    CustomerId = 1
+                },
+                new ProjectModel()
+                {
+                    Id = 2,
+                    Name = "Tesla Cyber Truck Project",
+                    Status = "Stuck",
+                    CustomerId = 2
+                },
+                new ProjectModel()
+                {
+                    Id = 3,
+                    Name = "Mark's Super Fantastic Fun Project",
+                    Status = "Work in Progress",
+                    CustomerId = 3
+                }
+            };
+        }
+    }
+}
---
+++ ViewModels/ProjectModel.cs
@@ -0,0 +1,13 @@
+namespace ContentHeroApi.ViewModels
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
